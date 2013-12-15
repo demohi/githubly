40dec5c282c4b7762ed852f7f7ae08599918e9db
mdemo <mengdesen09@qq.com>
2013-12-16 00:30:16 +0800

    add leaderboard lib and simple demo

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mdemo on 13-12-15.
+ */
+var leaderboard = require('./lib/leaderboard');
+
+var js_daily = leaderboard.request({language:'javascript',since:'monthly'},function(err,results){
+    if(!err){
+        console.log(results);
+    }
+});
